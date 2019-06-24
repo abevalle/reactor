65fac2c62c0e607f1bc78beab4d459202c4d6e2f
Abe Valle (WLS_SB) <me@abevalle.com>
2019-06-24 15:42:46 -0500

    got 24 to 12 hours working adding express functionality next

--- getIcs.js
+++ getIcs.js
@@ -1,26 +1,63 @@
-const fs = require('fs');
-const download = require('download');
+//NPM Packages
 const ical = require('ical')
 const date = new Date();
+const express = require('express')
+const app = express()
+const port - 3000;
+
 const url = 'http://technexus.yarooms.com/ical/2dbc59mhcllvp56ejxt2o72vt.ics'
 
 
+//Converts 24 hour time to 12 hour time
+function to12(time) {
+    if (time >= 13) {
+        var newTime = time-12
+    } else {
+        var newTime = time
+    }
+    return newTime
+}
+
+//downloads .ics file from ical
 ical.fromURL(url, {}, function(err, data) {
+    //converts ics to json and loops through json file.
     for (let k in data) {
         if (data.hasOwnProperty(k)) {
-            var ev = data[k];
+            const ev = data[k];
+            //sets todays date as a comparator
+            const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
 
-            var evstart = `${ev.start.getFullYear()}-${ev.start.getMonth()}-${ev.start.getDate()}`
-            var today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+            // This object contains event details
+            const event = {
+                "name": ev.summary,
+                "description": ev.description,
+                "location": ev.location,
+                "date": {
+                    'yyyy': ev.start.getFullYear(),
+                    'mm': ev.start.getMonth(),
+                    'dd': ev.start.getDate(),
+                },
+                "start": {
+                    "hh": to12(ev.start.getHours()),
+                    "mm": ev.start.getMinutes(),
+                    "ss": ev.start.getSeconds()
+                },
+                "end": {
+                    "hh": to12(ev.end.getHours()),
+                    "mm": ev.end.getMinutes(),
+                    "ss": ev.end.getSeconds()
+                }
+            }
+             
+            // This puts together event year, month, and day
+            const evStartDate = `${event.date.yyyy}-${event.date.mm}-${event.date.dd}`
+
+            // This if statement is to remove any events that are not today
+            if (evStartDate == today ) {
+                //This publishes to the api
+
+            }
         }
-
-        if (evstart !== '2019-5-24' ) {
-        } else {
-            const evName = ev.summary;
-            const evLocation = ev.location;
-            const evStart = ev.start;
-            const evEnd = ev.end;
-        }
-
     }
-})
+
+});
