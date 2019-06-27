1937ad756fa14df7d13e171341e441afc6163872
cassini <cassini@Command-Center.command.arbitrary.space>
2019-06-27 03:25:36 -0500

    adding postgresql

--- getIcs.js
+++ getIcs.js
@@ -1,55 +1,49 @@
-//NPM Packages
 const ical = require('ical')
-const date = new Date();
 
 const url = 'https://variadiq.yarooms.com/ical/eta87dxzdzd2bapmhhp313dhy.ics'
-//sets todays date as a comparator
-const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
-const events = {};
 
+let events = {};
 
-export function ics() {
-    //downloads .ics file from ical
-    ical.fromURL(url, {}, function (err, data) {
-        let i = 0
-        //converts ics to json and loops through json file.
-        for (let k in data) {
-            if (data.hasOwnProperty(k)) {                
-                const ev = data[k];
-                // This object contains event details
-                events[i++] = {
-                    "name": ev.summary,
-                    "description": ev.description,
-                    "location": ev.location,
-                    "date": {
-                        'yyyy': ev.start.getFullYear(),
-                        'mm': ev.start.getMonth(),
-                        'dd': ev.start.getDate(),
-                    },
-                    'start': {
-                        'hh': ev.start.getHours(),
-                        'mm': ev.start.getMinutes(),
-                        'ss': ev.start.getSeconds()
-                    },
-                    'end': {
-                        'hh': ev.end.getHours(),
-                        'mm': ev.end.getMinutes(),
-                        'ss': ev.end.getSeconds()
+function getCal() {
+    return new Promise(resolve => {
+        ical.fromURL(url, {}, function (err, data) {
+            let i = 0
+            //converts ics to json and loops through json file.
+            for (let k in data) {
+                if (data.hasOwnProperty(k)) {
+                    const ev = data[k];
+                    // This object contains event details
+                    events[i++] = {
+                        "name": ev.summary,
+                        "description": ev.description,
+                        "location": ev.location,
+                        "date": {
+                            'yyyy': ev.start.getFullYear(),
+                            'mm': ev.start.getMonth(),
+                            'dd': ev.start.getDate(),
+                        },
+                        'start': {
+                            'hh': ev.start.getHours(),
+                            'mm': ev.start.getMinutes(),
+                            'ss': ev.start.getSeconds()
+                        },
+                        'end': {
+                            'hh': ev.end.getHours(),
+                            'mm': ev.end.getMinutes(),
+                            'ss': ev.end.getSeconds()
+                        }
+
                     }
-
                 }
-
-                // This puts together event year, month, and day
-                //const evStartDate = `${events.date.yyyy}-${events.date.mm}-${events.date.dd}`
-
-                // This if statement is to remove any events that are not today
-                //if (evStartDate == '2019-5-27') {
-                    //return events
-                //}
-
             }
-        }
-        return events
+            resolve(events)
+        });
     });
+}
 
+module.exports = {
+        ics: async function() {
+        var result = await getCal();
+            return result
+    }
 }
--- sql.js
+++ sql.js
@@ -1,4 +1,6 @@
 const { Pool, Client } = require('pg')
+const { ics } = require('../reactor/getIcs');
+const date = new Date()
 const pool = new Pool({
     user: 'abe',
     host: 'localhost',
@@ -6,10 +8,38 @@ const pool = new Pool({
     password: 'popeysnotkfc',
     port: 5432
 });
+let events = "";
 
-console.log(ics)
+function cal() {
+    return new Promise(resolve => {
+        resolve(ics())
+    });
+}
 
-// pool.query('SELECT NOW()', (err, res) => {
-//     console.log(err, res)
-//     pool.end()
-// })
+async function call() {
+    const result = await cal();
+    const asnycEvents = result;
+    let data = asnycEvents;
+
+    for (let k in data) {
+        if (data.hasOwnProperty(k)) {
+            let ev = data[k]
+
+            let name = ev.name
+            let desc = ev.description
+            let location = ev.location
+            let date = `${ev.date.yyyy}-${ev.date.mm}-${ev.date.dd}`
+            let start = `${ev.start.hh}:${ev.start.mm}:${ev.start.ss}`
+            let end = `${ev.end.hh}:${ev.end.mm}:${ev.end.ss}`
+
+            //sets todays date as a comparator
+            //const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+
+            
+
+
+        }
+    }
+}
+
+call();
